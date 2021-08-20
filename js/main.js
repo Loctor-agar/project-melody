896b5a27cb0cebeb150f3b63311b7961cda778a4
Loctor-agar <khusan.mukhammetrakhimov@gmail.com>
2021-08-20 23:56:46 +0600

    add image create js code with jquery add svg code and data floor and css style animation

--- js/main.js
+++ js/main.js
@@ -1,15 +1,32 @@
 $(document).ready(function () {
   let currentFloor = 2;
+  let currentFlat = 40;
   let counterUp = $('.counter-up'); /* кнопка увелечения этажа */
   let counterDown = $('.counter-down'); /* кнопка увелечения этажа */
-  let floorPath = $('.home-image path')
-
+  let floorPath = $('.home-image path');
+  let flatsPath = $('.flats path')
+  let modal = $('.modal');
+  let modalCloseButton = $('.modal-close-button');
+  let viewFlatsButton = $('.view-flats')
   // функция при наведении мышкой на этаж
   floorPath.on('mouseover',function () { 
     floorPath.removeClass('current-floor'); /* удаляем активный класс у этажей */
     currentFloor = $(this).attr('data-floor'); /* получаем значение текущего этажа */
     $('.counter').text(currentFloor); /* записываем значение этажа в счетчик справа */
-   });
+  });
+
+  flatsPath.on('mouseover',function(){
+    flatsPath.removeClass('currentFlat');
+    currentFlat = $(this).attr('data-flat');
+    $('.current-flat').text(currentFlat);
+  });
+
+
+
+  floorPath.on('click', toggleModal); // при клике на этаж вызвать окно
+  modalCloseButton.on('click', toggleModal); 
+  viewFlatsButton.on('click', toggleModal);
+
   counterUp.on('click',function () { 
     if (currentFloor <  18){
       currentFloor++; 
@@ -27,5 +44,8 @@ $(document).ready(function () {
       floorPath.removeClass('current-floor')
       $(`[data-floor=${usCurrentFloor}]`).toggleClass('current-floor')
     }
-   })
+  })
+  function toggleModal(){
+    modal.toggleClass('is-open');
+  }
 });
